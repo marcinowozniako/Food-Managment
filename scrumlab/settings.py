129d5a4ae2d70511112faa6e5d4cef88afab5aed
marcinowozniako <marekboniek@gmail.com>
2022-04-25 23:36:31 +0200

    fix

--- scrumlab/settings.py
+++ scrumlab/settings.py
@@ -130,4 +130,4 @@ db_from_env = dj_database_url.config(
 
 DATABASES['default'].update(db_from_env)
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
